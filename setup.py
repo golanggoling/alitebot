3126449cb146797048899efc1789deea89b4649f
Damien Ginanjar <damienginanjar@gmail.com>
2019-10-30 15:18:03 +0700

    alitebot

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='alitebot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sidi',
+    author_email='',
+    description=''
+)
